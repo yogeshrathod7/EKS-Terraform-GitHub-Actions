8f0d0fac26ecb7d2c93341b9718e0b72f2a24dde
Yogesh Rathod <48018485+yogeshrathod7@users.noreply.github.com>
2025-09-25 12:58:13 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-yogesh-tf-bucket"
+    bucket         = "dev-yog-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
